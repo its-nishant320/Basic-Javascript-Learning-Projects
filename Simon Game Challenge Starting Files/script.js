21fec9a0800a0bfa5ceb25066528db65a43a94da
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-10-16 19:28:42 +0530

    qr code generator using native modules and npm

--- Simon Game Challenge Starting Files/script.js
+++ Simon Game Challenge Starting Files/script.js
@@ -1,13 +1,14 @@
+var gamePattern = [];
 var buttonColours = ['red','blue','yellow','green'];
 
 function newSequence() {
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColour = buttonColours*randomNumber;
+    var randomNumber = buttonColours[Math.floor(Math.random()*buttonColours.length)];
+    gamePattern.push(randomNumber);
+    
+    $("." + randomNumber).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("./sounds/"+ randomNumber +".mp3");
+    audio.play();
+    
 }
 
-
-
-
-var randomChosenColour = $(".btn").click(function() {
-    $(".btn").fadeOut(100).fadeIn(100);
-})
+newSequence();
