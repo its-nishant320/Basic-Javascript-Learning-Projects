39c941f42cded6075d2454a07ed599e5f1e74317
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-22 16:57:22 +0530

    Create promises.js

---
+++ async-javascript/promises.js
@@ -0,0 +1,56 @@
+const promiseOne = new Promise(function(resolve,reject) {
+    setTimeout(function(){
+        console.log("Async fun is called")
+        resolve();
+    },1000);
+})
+
+promiseOne.then(function() {
+    console.log("async resolved");
+})
+
+
+
+new Promise(function(resolve,reject) {
+    setTimeout(function(){
+        console.log("async task2 is called");
+        resolve();
+    })
+}).then(function() {
+    console.log("async2 resolved");
+})
+
+const promiseTwo = new Promise(function(resolve,reject) {
+    setTimeout(function(){
+        resolve({username:"Nishant", rollno:"AP22XXXXX944"})
+    },1000)
+})
+
+
+promiseTwo.then(function(user) {
+    console.log(user);
+})
+
+const promiseThree = new Promise(function(resolve,reject) {
+    let error = false;
+    if(!error) {
+        resolve({username:"Nishant",rollno:"AP22XXXXXX944"});
+    }
+    else {
+        reject("Something went wrong");
+    }
+})
+
+promiseThree.then(function(user) {
+    console.log("User details: ", user);
+    return user.rollno;
+})
+.then(function(username){
+    console.log("Roll no : ", username);
+})
+.catch(function(err) {
+    console.log(err);
+})
+.finally(function() {
+    console.log("Promise execution completed");
+})
