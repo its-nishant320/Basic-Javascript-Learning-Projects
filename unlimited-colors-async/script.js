3d4ae32129f110c2c2eb57c6da60664b6249adc8
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-23 16:22:34 +0530

    unlimited color change

---
+++ unlimited-colors-async/script.js
@@ -0,0 +1,33 @@
+
+const randomColor = function() {
+    let color = "#";
+    let hex = "0123456789ABCDEF";
+    for(let i=0; i<6; i++) {
+        color = color + hex[Math.floor(Math.random()*16)];
+    }
+return color;
+};
+
+let changing = function() {
+    document.body.style.backgroundColor = randomColor();
+    console.log("hey")
+}
+
+let intervalId;
+const changecolor = function () {
+    if(!intervalId) {
+       intervalId = setInterval(changing,1000);
+    }
+};
+
+const stopchanging = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+let a = document.querySelector("#start");
+a.addEventListener("click", changecolor);
+
+let b = document.querySelector("#stop");
+b.addEventListener("click", stopchanging)
+
